8ccd5c776f71c2cd53fd618c0a94454eab260cf2
Danqi Liao <danqiliao@gmail.com>
2016-05-21 17:42:14 -0500

    minor improvement

--- bayes_template.py
+++ bayes_template.py
@@ -40,12 +40,14 @@ class Bayes_Classifier:
           #moives-1-ID
           if filename[7] == '1': #negative reviews
               for token in tokens:
+                  token = token.lower()
                   if token in self.dict_negative:
                       self.dict_negative[token] = self.dict_negative[token] + 1
                   else:
                       self.dict_negative[token] = 1
           if filename[7] == '5': #positive reviews
               for token in tokens:
+                  token = token.lower()
                   if token in self.dict_positive:
                       self.dict_positive[token] = self.dict_positive[token] + 1
                   else:
@@ -62,6 +64,65 @@ class Bayes_Classifier:
         """Given a target string sText, this function returns the most likely document
         class to which the target string belongs (i.e., positive, negative or neutral).
         """
+
+        #get the list of training data
+        fileList = []
+        for f in os.walk("movies_reviews/"):
+          fileList = f[2]
+          break
+
+        #compute prior probability
+        positive = 0
+        negative = 0
+        for filename in fileList:
+            if filename[7] == '1': #negative reviews
+                negative = negative + 1
+            elif filename[7] == '5': #positive reviews
+                positive = positive + 1
+        prior_positive = positive / (positive+negative)
+        prior_negative = negative / (positive+negative)
+
+        #compute frequencies sum of both positive and negative dicts
+        #add one to smooth
+        freq_positive = 0
+        freq_negative = 0
+        for word, num in self.dict_positive.iteritems():
+            freq_positive = freq_positive + (num+1)
+        for word, num in self.dict_negative.iteritems():
+            freq_negative = freq_negative + (num+1)
+        freq_positive = float(freq_positive)
+        freq_negative = float(freq_negative)
+
+        #compute prob for the input text
+        prob_positive = 0
+        prob_negative = 0
+        for token in self.tokenize(sText):
+            token = token.lower()
+            if token in self.dict_positive:
+                prob_positive += math.log((self.dict_positive[token]/freq_positive))
+            else:#add one smoothing
+                prob_positive += math.log(1/freq_positive)
+
+            if token in self.dict_negative:
+                prob_negative += math.log((self.dict_negative[token]/freq_negative))
+            else:#add one smoothing
+                prob_negative += math.log(1/freq_negative)
+
+        #if prob_positive and prob_negative difference < 0.1, the comment is neutral
+        if abs(prob_positive - prob_negative) < 0.1:
+            print prob_positive-prob_negative
+            return "neutral"
+        if prob_positive - prob_negative > 0:
+            print prob_positive-prob_negative
+            return "positive"
+        elif prob_positive - prob_negative < 0:
+            print prob_negative-prob_positive
+            return "negative"
+
+
+
+
+
 
     def loadFile(self, sFilename):
         """Given a file name, return the contents of the file as a string."""
